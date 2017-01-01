d90c420e3efb99b8e80abbb08a8a5ec28c5cd043
Jonathan Wong <jkwong5@gmail.com>
2016-12-31 17:55:48 -0800

    edit message

--- public/scripts/apps.js
+++ public/scripts/apps.js
@@ -11,6 +11,18 @@
     $('#repoData').fadeToggle();
   });
 
+  //greeting
+  var now = new Date().getHours();
+  if(now < 12){
+    $('#greeting').append('<p>Good Morning,</p>');
+  }
+  else if(now < 17){
+    $('#greeting').append('<p>Good Afternoon,</p>');
+  }
+  else{
+    $('#greeting').append('<p>Good Evening,</p>');
+  }
+
 
   function Project(opts){
     this.name = opts.name;
@@ -48,49 +60,6 @@
       projectView.initIndexPage();
     }
   };
-    // else {
-    //   $.getJSON('data/skilldata.json', function(projects) {
-    //     Project.loadAll(projects);
-    //     localStorage.setItem('projects', JSON.stringify(projects));
-    //     projectView.initIndexPage();
-    //   });
-  // var repos = {};
-  // repos.all = [];
-  //
-  // repos.requestRepos = function(callback) {
-  //   $.ajax({
-  //     url: 'https://api.github.com/users/jkwong5/repos?per_page=10&sorted=updated',
-  //     type: 'GET',
-  //     // headers: {'authorization': 'token ' + githubtoken},
-  //     success: function(data){
-  //       data.forEach(function(ele){
-  //         console.log(ele);
-  //       });
-  //       repos.all = data;
-  //     }
-  //   }).done(callback);
-  // };
-  //
-  // repos.with = function() {
-  //   return repos.all.filter(function(repo) {
-  //     return repo.fork === false;
-  //   });
-  // };
-  //
-  // var repoView = {};
-  // var render = Handlebars.compile($('#repo-template').html());
-  // var ui = function(){
-  //   var $repos = $('#repos');
-  //   $repos.find('ul').empty();
-  //   $repos.show().siblings();
-  // };
-  //
-  // repoView.index = function() {
-  //   ui();
-  //   $('#repos ul').append(
-  //     repos.with('name').map(render)
-  //   );
-  // };
 
   function Repo(opts){
     this.name = opts.name;
