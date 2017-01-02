0fcca5c1adf5a36beb0448393188267818699f93
Jonathan Wong <jkwong5@gmail.com>
2017-01-01 16:41:30 -0800

    fix server.js

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-var requestProxy = require('express-request-proxy'),
-  express = require('express'),
-  port = process.env.PORT || 3000,
-  app = express();
+// var requestProxy = require('express-request-proxy'),
+var express = require('express');
+var app = express();
 
-var proxyGitHub = function(request, response) {
-  console.log('Routing GitHub request for', request.params[0]);
-  (requestProxy({
-    url: 'https://api.github.com/' + request.params[0],
-    headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
-  }))(request, response);
-};
-app.get('/github/*', proxyGitHub);
+// var proxyGitHub = function(request, response) {
+//   console.log('Routing GitHub request for', request.params[0]);
+//   (requestProxy({
+//     url: 'https://api.github.com/' + request.params[0],
+//     headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+//   }))(request, response);
+// };
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public/'));
 
-app.get('*', function(request, response) {
-  console.log('New request:', request.url);
-  response.sendFile('index.html', { root: '.' });
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-app.listen(port, function() {
-  console.log('Server started on port ' + port + '!');
+// app.get('/github/*', proxyGitHub);
+// app.get('*', function(request, response) {
+//   console.log('New request:', request.url);
+//   response.sendFile('index.html', { root: '.' });
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Server started on port ' + port + '!', app.get('port'));
 });
 
 var firebase = require('firebase/app');
